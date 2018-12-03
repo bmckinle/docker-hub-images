587312ff6b15213b212493b23b73dee1d16c38b5
Bryan McKinley <bryan.mckinley@pearson.com>
2018-12-03 15:51:55 -0600

    refactored to argbash but left untested

--- packer/deploy.sh
+++ packer/deploy.sh
@@ -2,32 +2,30 @@
 
 set -e
 
+echo "UNTESTED"
+
 base="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [[ $# -ne 1 ]]; then
-    echo "You must set a version number"
-    echo "./deploy.sh <packer version>"
-    exit 1
-fi
+source ${base}/deploy_argbash.sh
 
-version=$1
+version=${_arg_packerversion}
 dockerfile_version=$(grep PACKER_VERSION= ${base}/Dockerfile-light | cut -d= -f2)
 
 if [[ $version != $dockerfile_version ]]; then
-    echo "Version mismatch in 'Dockerfile-light'"
+    echo "Version mismatch in ${_arg_dockerfile}"
     echo "found ${dockerfile_version}, expected ${version}."
     echo "Make sure the versions are correct."
     exit 1
 fi
 
 echo "Building docker images for packer ${version}..."
-# docker build -f "${base}/Dockerfile-full" -t bmckinley/packeransible:full .
-docker build -f "${base}/Dockerfile-light" -t bmckinley/packeransible:light .
-docker tag bmckinley/packeransible:light bmckinley/packeransible:${version}
-docker tag bmckinley/packeransible:light bmckinley/packeransible:latest
+# docker build -f "${base}/Dockerfile-full" -t ${_arg_uri}:full .
+docker build -f "${base}/${_arg_dockerfile}" -t ${_arg_uri}:${_arg_tag} .
+docker tag ${_arg_uri}:${_arg_tag} ${_arg_uri}:${version}
+docker tag ${_arg_uri}:${_arg_tag} ${_arg_uri}:latest
 
 echo "Uploading docker images for packer ${version}..."
-docker push bmckinley/packeransible:${version}
-docker push bmckinley/packeransible:latest
-docker push bmckinley/packeransible:light
-# docker push bmckinley/packeransible:full
+docker push ${_arg_uri}:${version}
+docker push ${_arg_uri}:latest
+docker push ${_arg_uri}:${_arg_tag}
+# docker push ${_arg_uri}:full
